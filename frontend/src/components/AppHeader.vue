d62f4dcda6267d0a4344b488870d9116f9920ac0
nguyenhieunghia070101 <97340603+nguyenhieunghia070101@users.noreply.github.com>
2022-11-24 18:12:36 +0700

    update

---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light d-none d-lg-block" id="templatemo_nav_top">
+            <div class="w-100 d-flex justify-content-between">
+                <div >
+                    <i class="fa fa-envelope mx-2"></i>
+                    <a class="navbar-sm-brand text-decoration-none" href="mailto:info@company.com">Student@gmail.com</a>
+                    <i class="fa fa-phone mx-2"></i>
+                    <a class="navbar-sm-brand text-decoration-none" href="tel:010-020-0340">333-999-999</a>
+                </div>
+                <div>
+                    <a class="text-light" href="https://fb.com/templatemo" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="https://twitter.com/" target="_blank"><i class="fab fa-twitter fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin fa-sm fa-fw"></i></a>
+                </div>
+            </div>
+    </nav>
+    <div class="header">
+        <router-link to="Login.vue"> 
+                show rom car
+            </router-link> 
+    </div>
+</template>
+<style scoped> 
+    @import '@/assets/home.css'; 
+</style>
